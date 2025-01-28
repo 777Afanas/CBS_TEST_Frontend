e4a134c00b991d4b5814b5ed264b866990ac1502
Serhii Yatsenko <serhii2111@yahoo.com>
2025-01-29 01:19:14 +0200

    form_author

---
+++ src/js/author-form.js
@@ -0,0 +1,61 @@
+import { authors } from "./authors.js";
+
+let authorList = document.querySelector('#authorList');
+let saveButton = document.querySelector('#saveButton');
+let selectAuthor = null;
+
+for (let index = 0; index < authors.length; index++) {
+  const currentAuthor = authors[index];
+  let li = document.createElement('li');
+  li.textContent = currentAuthor.surname + ' ' + currentAuthor.nameA;
+  li.dataset.authorNumber = index;
+  authorList.append(li);
+}
+
+// Встановлення обробників
+authorList.addEventListener('click', function (e) {
+  if (e.target.tagName != 'LI') return;
+  selectAuthor = authors[e.target.dataset.authorNumber];
+  fillForm(selectAuthor);
+  selectListItem(e.target);
+});
+
+saveButton.addEventListener('click', function () {
+  updateAuthor(selectAuthor);
+});
+
+// Логіка
+function selectListItem(element) {
+  deselectAllListItems();
+  element.classList.add('selected');
+}
+
+function deselectAllListItems() {
+  let selectedItems = document.querySelectorAll('li.selected');
+  for (let i = 0; i < selectedItems.length; i++) {
+    const element = selectedItems[i];
+    element.classList.remove('selected');
+  }
+}
+
+function fillForm(selectAuthor) {
+  const form = document.forms[0];
+
+  form.surname.value = selectAuthor.surname;
+  form.nameA.value = selectAuthor.nameA;
+  form.patronymic.value = selectAuthor.patronymic;
+  form.dob.value = selectAuthor.dob;
+//   form.email.value = selectAuthor.email;
+//   form.age.value = selectAuthor.age;
+}
+
+function updateAuthor(selectAuthor) {
+  const form = document.forms[0];
+
+  selectAuthor.surname = form.surname.value;
+  selectAuthor.nameA = form.nameA.value;
+  selectAuthor.patronymic = form.patronymic.value;
+  selectAuthor.dob = form.dob.value;
+//   selectAuthor.email = form.email.value;
+//   selectAuthor.age = form.age.value;
+}
---
+++ src/js/authors.js
@@ -0,0 +1,20 @@
+export const authors = [
+  {
+    surname: 'Clements',
+    nameA: 'Golden',
+    patronymic: 'GG',
+    dob: 24,
+  },
+  {
+    surname: 'Hebert',
+    nameA: 'Francis',
+    patronymic: 'FF',
+    dob: 24,
+  },
+  {
+    surname: 'Cross',
+    nameA: 'Hale', 
+    patronymic: 'LL',
+    dob: 24,
+  },
+];
