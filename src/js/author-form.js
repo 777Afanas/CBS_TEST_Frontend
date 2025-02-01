0d954f90a258cd43c438ab8c85be0ce1618f404f
Serhii Yatsenko <serhii2111@yahoo.com>
2025-02-01 17:36:35 +0200

    form_book

--- src/js/author-form.js
+++ src/js/author-form.js
@@ -1,15 +1,18 @@
-import { authors } from "./authors.js";
+import { authors } from './authors.js';
 
-let authorList = document.querySelector("#authorList");
-let saveButton = document.querySelector("#saveButton");
+let authorList = document.querySelector('#authorList');
+let saveButton = document.querySelector('#saveButton');
 let selectAuthor = null;
-let form = document.querySelector('#output'); 
+let form = document.querySelector('#output');
+let bookForm = document.querySelector('#book');
 
 // const STORAGE_KEY = "authors-item";
-let keykey = Date.now();
+let key = Date.now();
 
 const catalog = [];
+const bibliography = [];
 form.addEventListener('submit', onFormSubmit);
+bookForm.addEventListener('submit', onBookFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
@@ -19,12 +22,8 @@ function onFormSubmit(event) {
   let data = Object.fromEntries(formData.entries());
   console.log(data);
 
-  event.currentTarget.reset();
-  // const catalog = [];
-
-  // console.log(nums);
   const nums = catalog.push(data);
-  // localStorage.setItem(keykey, JSON.stringify(catalog));
+  localStorage.setItem(key, JSON.stringify(catalog));
 
   document.querySelector(
     '.content'
@@ -39,22 +38,51 @@ function onFormSubmit(event) {
   // <td>${dob}</td> `;
   // document.querySelector('.authors').appendChild(row);
 
-  console.log(catalog);
-  for (var key in catalog) {
-    // console.log(data[key]);
-    // const { surname, nameA, patronymic, dob } = data;
+  for (let i = 0; i < catalog.length; i++) {
     let row = document.createElement('tr');
     row.innerHTML = `
-    <td>${surname}</td>
-    <td>${nameA}</td>
-    <td>${patronymic}</td>
-    <td>${dob}</td> `;
+    <td>${catalog[i].surname}</td>
+    <td>${catalog[i].nameA}</td>
+    <td>${catalog[i].patronymic}</td>
+    <td>${catalog[i].dob}</td> `;
+
     document.querySelector('.authors').appendChild(row);
   }
+  // }
+
   event.currentTarget.reset();
   // form.reset();
 }
 
+function onBookFormSubmit(event) {
+  event.preventDefault();
+
+const formData = new FormData(event.currentTarget);
+console.log(formData);
+let data = Object.fromEntries(formData.entries());
+console.log(data);
+
+const nums = bibliography.push(data);
+// localStorage.setItem(key, JSON.sringify(catalog));
+
+document.querySelector(
+  '.books'
+  ).innerHTML = `<table class="record"></table>`;
+  
+  for (let i = 0; i < bibliography.length; i++) {
+    let row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${bibliography[i].title}</td>
+    <td>${bibliography[i].paging}</td>     
+    <td>${bibliography[i].genre}</td> `;
+
+    document.querySelector('.record').appendChild(row);
+  }
+  // }
+
+  event.currentTarget.reset();
+
+}
 // const makeAuthorsTableRowMarkup = data => {
 // const { surname, nameA, patronymic, dob } = data;
 
@@ -63,7 +91,7 @@ function onFormSubmit(event) {
 //     <td>${surname}</td>
 //     <td>${nameA}</td>
 //     <td>${patronymic}</td>
-//     <td>${dob}</td>     
+//     <td>${dob}</td>
 //   </tr>
 //   `;
 // };
@@ -78,8 +106,6 @@ function onFormSubmit(event) {
 
 // console.log(transactionTableRowsMarkup);
 
-
-
 for (let index = 0; index < authors.length; index++) {
   const currentAuthor = authors[index];
   let li = document.createElement('li');
@@ -87,16 +113,13 @@ for (let index = 0; index < authors.length; index++) {
   li.dataset.authorNumber = index;
   authorList.append(li);
   // console.log(li);
-  
 }
 
 // Встановлення обробників
 authorList.addEventListener('click', function (e) {
-
-
-  if (e.target.tagName != "LI") return;
+  if (e.target.tagName != 'LI') return;
   selectAuthor = authors[e.target.dataset.authorNumber];
-  
+
   fillForm(selectAuthor);
   selectListItem(e.target);
 });
@@ -121,13 +144,13 @@ function deselectAllListItems() {
 
 function fillForm(selectAuthor) {
   const form = document.forms[0];
-  
+
   form.surname.value = selectAuthor.surname;
   form.nameA.value = selectAuthor.nameA;
   form.patronymic.value = selectAuthor.patronymic;
   form.dob.value = selectAuthor.dob;
-//   form.email.value = selectAuthor.email;
-//   form.age.value = selectAuthor.age;
+  //   form.email.value = selectAuthor.email;
+  //   form.age.value = selectAuthor.age;
 }
 
 function updateAuthor(selectAuthor) {
@@ -137,28 +160,6 @@ function updateAuthor(selectAuthor) {
   selectAuthor.nameA = form.nameA.value;
   selectAuthor.patronymic = form.patronymic.value;
   selectAuthor.dob = form.dob.value;
-//   selectAuthor.email = form.email.value;
-//   selectAuthor.age = form.age.value;
+  //   selectAuthor.email = form.email.value;
+  //   selectAuthor.age = form.age.value;
 }
-
-
-// const form = document.querySelector('.js-register-form');
-
-// authorList.addEventListener('submit', onFormSubmit);
-
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-
-// console.dir(event.currentTarget.elements);
-
-
-//   const formData = new FormData(event.currentTarget);
-
-//   console.log(formData);
-
-//   formData.forEach((value, name) => {
-//     console.log('onFormSubmit -> name', name);
-//     console.log('onFormSubmit -> value', value);
-//   });
-// }
